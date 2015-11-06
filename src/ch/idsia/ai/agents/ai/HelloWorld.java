ecfcf90dba27129069cf0c8623ea1b3d362d271d
nathanielshak <nathaniel.shak@gmail.com>
2015-11-05 21:23:28 -0800

    added alot of printy stuff

--- src/ch/idsia/ai/agents/ai/HelloWorld.java
+++ src/ch/idsia/ai/agents/ai/HelloWorld.java
@@ -18,19 +18,46 @@ public class HelloWorld extends BasicAIAgent implements Agent
         action = new boolean[Environment.numberOfButtons];// Empty action
     }
 
-    public boolean[] getAction(Environment observation)
-    {
-        byte[][] wee = observation.getEnemiesObservation();
-        int len = wee.length;
+    private void print2dArray(byte[][] arr){
+        int len = arr.length;
         for(int i = 0; i < len; i++){
-            int width = wee[i].length;
+            int width = arr[i].length;
             String a = "";
             for(int j = 0; j < width; j++){
-                a += wee[i][j];
+                a += arr[i][j];
             }
             System.out.println(a);
         }
+    }
+
+    private void print1dArray(float[] arr){
+        int len = arr.length;
+        String a = "";
+        for(int i = 0; i < len; i ++){
+            a += arr[i] + "  ";
+        }
+        System.out.println(a);
+    }
+
+    public boolean[] getAction(Environment observation)
+    {
+        byte[][] enemies = observation.getEnemiesObservation();
+        byte[][] completeObv = observation.getCompleteObservation();
+        byte[][] levelObv = observation.getLevelSceneObservation();
+        System.out.println("enemies");
+        print2dArray(enemies);
+        System.out.println("complete");
+        print2dArray(completeObv);
+        System.out.println("levelObv");
+        print2dArray(levelObv);
+        float[] mposition = observation.getMarioFloatPos();
+        float[] eposition = observation.getEnemiesFloatPos();
+        System.out.println("mario position");
+        print1dArray(mposition);
+        System.out.println("enemies position");
+        print1dArray(eposition);
         action[Mario.KEY_RIGHT] = true;
+        action[Mario.KEY_JUMP] =  observation.mayMarioJump() || !observation.isMarioOnGround();
         return action;
     }
 
